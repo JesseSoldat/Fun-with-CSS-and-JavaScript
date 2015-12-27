f800555516a1e47607f3e70b2598650b25b74108
Jesse Soldat <jsoldat@hotmail.com>
2015-12-27 08:47:33 -0500

    can shrink the box as well. also after clicking the buttons each button is disabled

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function changeStyle (element, clr) {
  var elem = document.getElementById(element);
  var btn = document.getElementById('btn');
+ var btn2 = document.getElementById('btn2');
+
+ btn.setAttribute('disabled', 'true');
 
  elem.style.transition = 'all 3.0s linear 0s';
  elem.style.background = clr;
@@ -11,6 +14,27 @@ function changeStyle (element, clr) {
  elem.style.fontSize = '3em';
  elem.style.fontWeight = 'bolder';
 
+ 
+ btn2.style.opacity = '1';
 
- btn.setAttribute('disabled', true);
+ setTimeout( function() {
+  btn2.removeAttribute('disabled');
+ },3000);
+ 
+}
+
+function changeStyle2(element) {
+  var elem2 = document.getElementById(element);
+  elem2.style.transition = 'all 3.0s linear 0s';
+  elem2.style.background = 'pink';
+  elem2.style.border = '2px dashed purple';
+  elem2.style.width = '100px';
+  elem2.style.height = '50px';
+  elem2.innerHTML = "<h5>"+"I'm Shrinking!"+"</h5>";
+  elem2.style.fontSize = '1em';
+  elem2.style.fontWeight = 'normal';
+  elem2.style.fontFamily = 'cursive';
+
+  btn2.setAttribute('disabled', true);
+
 }
