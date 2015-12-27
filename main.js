a655aa7d3942450119ae14645360096067b47291
Jesse Soldat <jsoldat@hotmail.com>
2015-12-27 08:26:02 -0500

    applied multiple styles and also changed the text in the box on click

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ function changeStyle (element, clr) {
  var elem = document.getElementById(element);
  var btn = document.getElementById('btn');
 
- elem.style.transition = 'background 3.0s linear 0s';
+ elem.style.transition = 'all 3.0s linear 0s';
  elem.style.background = clr;
+ elem.style.border = '5px solid black';
+ elem.style.width = '600px';
+ elem.style.height = '300px';
+ elem.innerHTML = "<h3>"+"Now you've done it!"+"</h3>";
+ elem.style.fontSize = '3em';
+ elem.style.fontWeight = 'bolder';
+
 
  btn.setAttribute('disabled', true);
 }
