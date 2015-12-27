e4e65762ee4d015d0ed889ac61c7e458e1bf2e32
Jesse Soldat <jsoldat@hotmail.com>
2015-12-27 10:32:11 -0500

    finished the first box

--- main.js
+++ main.js
@@ -1,40 +1,84 @@
-function changeStyle (element, clr) {
- var elem = document.getElementById(element);
+ var elem = document.getElementById('box1');
  var btn = document.getElementById('btn');
  var btn2 = document.getElementById('btn2');
+ var text = document.getElementById('firstText');
+ var leprechaun = document.getElementById('leprechaun');
+ var rainbow = document.getElementById('rainbow');
+ var gold = document.getElementById('gold');
 
+function changeStyle () {
  btn.setAttribute('disabled', 'true');
 
  elem.style.transition = 'all 3.0s linear 0s';
- elem.style.background = clr;
+ text.innerHTML = "Now you've done it!";
+ elem.style.background = 'red';
  elem.style.border = '5px solid black';
  elem.style.width = '600px';
- elem.style.height = '300px';
- elem.innerHTML = "<h3>"+"Now you've done it!"+"</h3>";
+ elem.style.height = '400px';
+
+ leprechaun.style.transition = 'all 3.0 linear 0s';
+ leprechaun.style.opacity = 0.0;
+ 
  elem.style.fontSize = '3em';
  elem.style.fontWeight = 'bolder';
 
- 
  btn2.style.opacity = '1';
 
  setTimeout( function() {
+  rainbowAppear();
+
   btn2.removeAttribute('disabled');
+
  },3000);
  
 }
 
-function changeStyle2(element) {
-  var elem2 = document.getElementById(element);
-  elem2.style.transition = 'all 3.0s linear 0s';
-  elem2.style.background = 'pink';
-  elem2.style.border = '2px dashed purple';
-  elem2.style.width = '100px';
-  elem2.style.height = '50px';
-  elem2.innerHTML = "<h5>"+"I'm Shrinking!"+"</h5>";
-  elem2.style.fontSize = '1em';
-  elem2.style.fontWeight = 'normal';
-  elem2.style.fontFamily = 'cursive';
+function changeStyle2() {
 
-  btn2.setAttribute('disabled', true);
+  var first = function() {
+    var elem2 = document.getElementById('box1');
 
+    btn2.setAttribute('disabled', true);
+
+    elem2.style.transition = 'all 3.0s linear 0s';
+    document.getElementById('firstText')
+    firstText.innerHTML = "";
+    
+    elem2.style.background = 'pink';
+    elem2.style.border = '2px dashed purple';
+    elem2.style.width = '100px';
+    elem2.style.height = '50px';
+
+   setTimeout(function(){
+      rainbowFade();
+    },4000);
+    
+    setTimeout(function(){
+      second();
+    },4000);
+  };
+  first();
 }
+
+var rainbowAppear = function () {
+rainbow.style.transition = 'opacity 3.0 linear 0s';
+rainbow.style.opacity = 1.0;
+};
+
+var rainbowFade = function () {
+rainbow.style.transition = 'opacity 3.0 linear 0s';
+rainbow.style.opacity = 0.0;
+};
+
+var second = function() {
+  var secondText = document.getElementById('firstText');
+  secondText.innerHTML = "I've shrunk!"
+  secondText.style.fontSize = '14px';
+  secondText.style.top = '20px';
+  secondText.style.left = 0;
+  secondText.style.fontFamily = 'cursive';
+    
+  gold.style.transition = 'all 3.0s linear 0s';
+  gold.style.opacity = 1.0; 
+}
+
